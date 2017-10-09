1a29bbf5e2ac9d3d3d02eb83a1903e8902fcbdf3
rlghd7414 <8023kk@naver.com>
2017-10-10 01:39:32 +0900

    201320618 gihong

--- src/commands.c
+++ src/commands.c
@@ -1,12 +1,14 @@
 #include <string.h>
-
+#include <unistd.h>
 #include "commands.h"
+#include <stdio.h>
+char dirname[200];
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
+chdir("argv[1]");
   return 0;
 }
 
@@ -14,17 +16,23 @@ int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
+  printf("%s\n",dirname);
 
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+if(chdir(argv[1])) 
   return 1;
+
+else
+return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+  if(getcwd(dirname,200)==dirname)
   return 1;
+
+
+return 0;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,72 @@
 #include "utils.h"
+#include <stdlib.h>
+#include <string.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+int x=0;
+int y=0;
+int i=0;
+int j=0;
+int p=0;
+int range=0;
+int max_range=0;
+int word=0;
+
+*argc=1;
+
+while(1)
+{
+if (command[i]==' ')
+{
+range++;
+if(range>max_range)
+max_range=range;
+range=0;
+word++;
+}
+else if(command[i]=='\0')
+{
+if(range>max_range)
+max_range=range;
+word++;
+}
+else
+{
+range++;
+}
+
+if(command[i]=='\0')
+break;
+i++;
+}
+
+
+*argv=(char**)malloc(sizeof(char*)*word);
+for(i=0;i<word;i++)
+{
+(*argv)[i]=(char*)malloc(sizeof(char)*max_range);
+
+}
+
+for(i=0;command[i]!='\0';i++)
+{
+if(command[i]==' ')
+{
+(*argv)[(*argc)-1][p++]='\0';
+(*argc)++;
+p=0;
+}
+else if (command[i]=='\0')
+{
+(*argv)[(*argc)-1][p++]='\0';
+}
+else
+{
+(*argv)[(*argc)-1][p++]=command[i];
+}
+}
+return ;
+
 }
